19c028d915fbdbff4e2d0c0b9ac4ff118c7ca820
ARYAN <aryan@cse.iitb.ac.in>
2017-09-28 22:07:42 +0530

    created update, add sequence and finder in grid.js

--- grid.js
+++ grid.js
@@ -37,7 +37,7 @@ function Grid(nora, noca){
 			each_row[i] = undefined;
 		}
 		for(var i = 0; i < grid_num_row; i++){
-			grid[i] = each_row.slice(0);
+			board[i] = each_row.slice(0);
 		}
 	}
 	this.add_player(pid) = function(){
@@ -67,7 +67,7 @@ function Grid(nora, noca){
 			cppx.push(Math.floor(grid_num_col/2));
 			cppy.push(Math.floor(grid_num_row / 2));
 			cppdiri.push([0, 1]);
-			cppiniposi.push([cppx[0], y[0]]);
+			cppiniposi.push([cppx[0], cppy[0]]);
 		}
 		else if (cpplen == 2){
 			cppx.push(Math.floor(grid_num_col/4));
@@ -75,8 +75,8 @@ function Grid(nora, noca){
 			cppy.push(Math.floor(grid_num_row / 2));
 			cppdiri.push([0, 1]);
 			cppdiri.push([0, 1]);
-			cppiniposi.push([cppx[0], y[0]]);
-			cppiniposi.push([cppx[1], y[0]]);
+			cppiniposi.push([cppx[0], cppy[0]]);
+			cppiniposi.push([cppx[1], cppy[0]]);
 		}
 		for(var i = 0; i < cpplen; ++i){
 			create_and_set_player_at(self.ini_list_pid_and_pnts[i][0], cppiniposi[i], cppdiri[i]);
@@ -116,11 +116,38 @@ function Grid(nora, noca){
 		// else:
 		actual_current_up_no++;
 		var update_update_number = current_update_number;
-		if (!seq_of_unprocessed_moves.empty()){
+		if (seq_of_unprocessed_moves.length > 0){
 			update_update_number = seq[0][0];
 		}
 		// incomplete hai, due to aalas
+		//finder(what, in_this_list, ith_elem_of_each_elem)
+		var i = finder(update_update_number, seq_of_moves, 0);
+		if (i != seq_of_moves.length) {
+			// a1 = [1,2,3,4,5];
+			// a2 = [21,22];
+			// a1.splice.apply(a1, [2, 0].concat(a2));
+			// console.log(a1); // [1, 2, 21, 22, 3, 4, 5];
+			// moving all elem from i onwards into the beginning of seq_of_unprocessed_moves
 
+			temp_array = seq_of_moves.splice(i, seq_of_moves.length - i);
+			seq_of_unprocessed_moves.splice.apply(seq_of_unprocessed_moves, [0, 0].concat(temp_array));
+		}
+
+		while(seq_of_unprocessed_moves.length > 0){
+			if (seq_of_unprocessed_moves.length > 1) current_update_number = seq[1][0];
+			else{
+				current_update_number = actual_current_up_no;
+			}
+			oracle(seq_of_unprocessed_moves[0]);
+			seq_of_unprocessed_moves.shift();
+		}
+
+		if (actual_current_up_no != current_update_number){
+			cur_n = current_update_number;
+			current_update_number = actual_current_up_no;
+			oracle_extrapolate(cur_n);
+		}
+		return 1;
 	}
 
 	this.get_killed_list(){
@@ -129,7 +156,34 @@ function Grid(nora, noca){
 
 	this.add_sequence(seq){
 		should_update = false;
-		//again ditched due to aalas
+		var i = finder(seq[0], seq_of_moves, 0);
+		if (i == seq_of_moves.length){
+			if (seq_of_unprocessed_moves.length > 0){
+				var j = finder(seq[0], seq_of_unprocessed_moves, 0);
+				seq_of_unprocessed_moves.splice(j, 0, seq);
+				should_update = true;
+				return;
+			}
+			else{
+				seq_of_unprocessed_moves.push(seq);
+				should_update = true;
+				return;
+			}
+		}
+		else{
+			// if (seq_of_unprocessed_moves.length > 0){
+			// 	var j = finder(seq[0], seq_of_unprocessed_moves, 0);
+			// 	seq_of_unprocessed_moves.splice(j, 0, seq);
+			// }
+			// else{
+			// 	seq_of_unprocessed_moves.push(seq);
+			// }
+			temp_array = seq_of_moves.splice(i, seq_of_moves.length - i);
+			seq_of_unprocessed_moves.splice.apply(seq_of_unprocessed_moves, [0, 0].concat(temp_array));
+			seq_of_unprocessed_moves.unshift(seq); // inserted at the beginning
+			should_update = true;
+			return;
+		}
 	}
 
 	this.make_ready_for_update(){
@@ -147,5 +201,27 @@ function Grid(nora, noca){
 	}
 	this.oracle_clear_board_from(update_no){
 		
+	}
+	this.finder(what, in_this_list, ith_elem_of_each_elem){
+		//finds the smallest index r such that ith_elem_of_each_elem of elem at index at r
+		// is >= what and returns it
+		if (in_this_list.length == 0) return 0;
+		if (last elem is < what) return in_this_list.length;
+
+		function finder_help(start_ind, end_ind){
+			mid_ind = Math.floor((start_ind + end_ind)/2); 
+			if (in_this_list[mid_ind] < what){
+				return finder_help(mid_ind+1, end_ind);
+			}
+			else if (mid_ind == 0 || in_this_list[mid_ind-1] < what){
+				return mid_ind;
+			}
+			else{
+				return finder_help(start_ind, mid_ind);
+			}
+		}
+
+		res = finder_help(0, in_this_list.length);
+		return res;
 	}
 }
