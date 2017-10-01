48b758fdbb927f1b1eb92e0c790cac2c4cf1c42a
sustarun <30590368+sustarun@users.noreply.github.com>
2017-10-02 01:11:58 +0530

    changed respawn_dead

--- grid.js
+++ grid.js
@@ -268,7 +268,7 @@ function Grid(nora, noca){
 			if (p.last_killed >= from_this_time){
 				if (!birth_list.includes(p)){
 					birth_list.push(p);
-					var index = killed_gplayers.findIndex(p);
+					var index = killed_gplayers.indexOf(p);
 					killed_gplayers.splice(index,1);
 				}
 			}
